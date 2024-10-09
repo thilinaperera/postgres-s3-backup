37ee7bbc07563fc66bd96e5be2f1010b5d081660
Thilina Perera <thilina@thilina.lk>
2024-10-09 17:28:01 +0000

    fix: Bug fix on cron expression

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ fi
 if [ "${SCHEDULE}" = "**None**" ]; then
   sh backup.sh
 else
-  echo -e "SHELL=/bin/sh\n${SCHEDULE} /bin/sh /backup.sh" >/etc/crontabs/root
+  echo -e "SHELL=/bin/sh\n${SCHEDULE} root /bin/sh /backup.sh" >/etc/crontabs/root
   exec go-crond /etc/crontabs/root
 fi
